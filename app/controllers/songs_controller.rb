59e6ed6d5cc2ac2e4e4462a615c3c2a868b4431b
dinnosaur <dinnoroni@gmail.com>
2020-03-23 10:46:03 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -9,9 +9,12 @@ class SongsController < ApplicationController
 
   def new
     @song = Song.new
+   
   end
 
   def create
+    
+    
     @song = Song.new(song_params)
 
     if @song.save
@@ -47,7 +50,7 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title)
+    params.require(:song).permit(:title, :genre_id , :artist_name, notes:[:attributes_0_content,:attributes_1_content])
   end
 end
 
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,52 @@
 class Song < ActiveRecord::Base
   # add associations here
+  belongs_to :artist 
+  belongs_to :genre 
+  has_many :notes 
+
+  def notes=(notes)
+    
+      notes.each do |key, value| 
+      note = Note.create(content: value, song: self)
+      self.notes << note 
+     
+      end
+
+  end 
+
+  def artist_name=(name)
+     artist =  Artist.find_or_create_by(name:name)
+     self.artist = artist 
+      
+  end 
+
+  def artist_name 
+    self.artist ? self.artist.name : nil
+  end
+
+
+#   def song_genre_id=(id)
+#     genre =  Genre.find(id)
+#     self.genre = genre
+     
+#  end 
+
+#  def song_genre_id 
+#   self.genre ? self.genre.name : nil
+
+# end
+
+  
+  
+  
+
+
+  # def add_note
+  #   self.notes? self.notes : nil
+  # end
+
+
+
+
+
 end
